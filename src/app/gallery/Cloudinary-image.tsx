be22f40709619b1cbe3e5fc298d165811bbed8fa
ghufranshaiikh <officialghufranali@gmail.com>
2024-01-11 16:33:36 +0500

    fav

--- src/app/gallery/Cloudinary-image.tsx
+++ src/app/gallery/Cloudinary-image.tsx
@@ -1,7 +1,12 @@
 'use client'
+import Heart from '@/components/icons/heart';
 import { CldImage } from 'next-cloudinary';
 
 export default function CloudinaryImage(props:any) {
-    return <CldImage {...props}/>
-    
+    return (
+        <div className='relative'>
+    <CldImage {...props}/>
+     <Heart className='absolute top-2 right-2' />
+    </div>
+    )
 }
